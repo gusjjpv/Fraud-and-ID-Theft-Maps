ddff1c003633797db238bf9f56f448af508fd29a
gusDev <joaogustavolima084@gmail.com>
2025-02-18 19:35:09 -0300

    refactor: fraud facts code

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,14 @@ import pandas as pd
 APP_TITLE = "Relatório de Fraude e Roubo de Identidade"
 APP_SUB_TITLE = "Fonte: Comissão Federal de Comércio"
 
+def display_fraud_facts(df, year, quarter, state_name, report_type, field_name, metric_title):
+    df = df[(df['Year'] == year) & (df['Quarter'] == quarter) & (df['Report Type'] == report_type)]
+    if state_name:
+        df = df[df['State Name'] == state_name]
+    df.drop_duplicates(inplace=True)
+    total = df[field_name].sum()
+    st.metric(metric_title, '{:,}'.format(total))
+
 def main():
     st.set_page_config(APP_TITLE)
     st.title(APP_TITLE)
@@ -16,19 +24,11 @@ def main():
     quarter = 1
     state_name = "Texas"
     report_type = "Other"
-    field_name = 'State Fraud/Other Count'
     metric_title = f'# of {report_type} Reports'
     
-    df = df[(df['Year'] == year) & (df['Quarter'] == quarter) & (df['Report Type'] == report_type)]
-    if state_name:
-        df = df[df['State Name'] == state_name]
-    df.drop_duplicates(inplace=True)
-    total = df[field_name].sum()
-    st.metric(metric_title, '{:,}'.format(total))
+    display_fraud_facts(df, year, quarter, state_name, report_type, 'State Fraud/Other Count', metric_title)
     
-    st.write(df.shape)
-    st.write(df.head())
-    st.write(df.columns)
+    
     
     #DISPLAY FILTERS AND MAP
     
