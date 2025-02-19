79fee9351347b5c19c2ca521b8d33cc114cb457f
gusDev <joaogustavolima084@gmail.com>
2025-02-19 19:53:26 -0300

    Display Median Loss

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,15 @@ import pandas as pd
 APP_TITLE = "Relatório de Fraude e Roubo de Identidade"
 APP_SUB_TITLE = "Fonte: Comissão Federal de Comércio"
 
-def display_fraud_facts(df, year, quarter, state_name, report_type, field_name, metric_title, number_format='${:,}'):
+def display_fraud_facts(df, year, quarter, state_name, report_type, field_name, metric_title, number_format='${:,}', is_median=False):
     df = df[(df['Year'] == year) & (df['Quarter'] == quarter) & (df['Report Type'] == report_type)]
     if state_name:
         df = df[df['State Name'] == state_name]
     df.drop_duplicates(inplace=True)
-    total = df[field_name].sum()
+    if is_median:
+        total = df[field_name].sum() / len(df) if len(df) else 0
+    else:
+        total = df[field_name].sum()
     st.metric(metric_title, number_format.format(round(total)))
 
 def main():
@@ -19,20 +22,25 @@ def main():
     
     #LOAD DATA
     df_fraud = pd.read_csv("data/AxS-Fraud Box_Full Data_data.csv")
+    df_median = pd.read_csv("data/AxS-Median Box_Full Data_data.csv")
     df_loss = pd.read_csv("data/AxS-Losses Box_Full Data_data.csv")
     
     year = 2022
     quarter = 1
     state_name = ""
     report_type = "Fraud"
-    
-    display_fraud_facts(df_fraud, year, quarter, state_name, report_type, 'State Fraud/Other Count', f'# of {report_type} Reports', number_format='{:,}')
-    display_fraud_facts(df_loss, year, quarter, state_name, report_type, 'Total Losses', 'Total $  Loss')
-
 
     #DISPLAY FILTERS AND MAP
     
     #DISPLAY METRICS
+    
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        display_fraud_facts(df_fraud, year, quarter, state_name, report_type, 'State Fraud/Other Count', f'# of {report_type} Reports', number_format='{:,}')
+    with col2:
+        display_fraud_facts(df_median, year, quarter, state_name, report_type, 'Overall Median Losses Qtr', 'Median $  Loss', is_median=True)
+    with col3:
+        display_fraud_facts(df_loss, year, quarter, state_name, report_type, 'Total Losses', 'Total $  Loss')
 
 if __name__ == "__main__":
     main()
